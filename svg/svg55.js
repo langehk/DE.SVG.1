0c484fcf284679cc53834119c3e9a5c8aeeca648
nadiaskau <nadiaskau@hotmail.com>
2020-10-27 12:06:58 +0100

    Lavet den sidste svg - flere bevægende cirkler med DRY princip

--- svg/svg55.js
+++ svg/svg55.js
@@ -7,7 +7,7 @@ export const $ = function (foo) {return document.getElementById(foo);}
  * widx: width of animated svg
  * wp: viewport (html elm) holding the svgelm
  */
-export const anim = function (svgelm, widx, dx, wp) {
+export function anim (svgelm, widx, dx, wp) {
     let vb = window.getComputedStyle($(wp)); //Vi får computed css-styles, så vi kan kalde dem
     let xmax = parseInt(vb.getPropertyValue('width')); //Vi henter css-width
     let ymax = parseInt(vb.getPropertyValue('height')); //Vi henter css-height
@@ -31,6 +31,6 @@ export const anim = function (svgelm, widx, dx, wp) {
         $(svgelm).style.top  = top + dx + 'px';
     }, 10);
 
-    console.log(ymax);
+    console.log($(svgelm));
 
 }
